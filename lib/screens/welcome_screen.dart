e7538699ac16712f2f1e31b7e4c363d0bdded833
Mehmet Yıldız <yildzmehmet7@gmail.com>
2019-09-27 11:06:31 +0300

    temp

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flash_chat/components/rounded_button.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'login_screen.dart';
@@ -78,53 +79,13 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
             SizedBox(
               height: 48.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Hero(
-                tag: "loginBtn",
-                child: Material(
-                  elevation: 5.0,
-                  color: Colors.lightBlueAccent,
-                  borderRadius: BorderRadius.circular(30.0),
-                  child: MaterialButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, LoginScreen.id);
-                      //Go to login screen.
-                    },
-                    minWidth: 200.0,
-                    height: 42.0,
-                    child: Text(
-                      'Log In',
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Hero(
-                tag: "registerBtn",
-                child: Material(
-                  color: Colors.blueAccent,
-                  borderRadius: BorderRadius.circular(30.0),
-                  elevation: 5.0,
-                  child: MaterialButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, RegistrationScreen.id);
-                      //Go to registration screen.
-                    },
-                    minWidth: 200.0,
-                    height: 42.0,
-                    child: Text(
-                      'Register',
-                    ),
-                  ),
-                ),
-              ),
-            ),
+            RoundedButton(title: "Login",callback: (){Navigator.pushNamed(context, LoginScreen.id);},),
+            RoundedButton(title: "Register", callback: (){Navigator.pushNamed(context, RegistrationScreen.id);},),
           ],
         ),
       ),
     );
   }
 }
+
+
